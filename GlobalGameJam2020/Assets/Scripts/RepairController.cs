75e2a49cb84a7fc8810900774ad231e30b097b0b
Bernardo <behornerl@gmail.com>
2020-02-01 02:07:45 -0300

    Modificando o nome do Repair.cs para RepairController.cs

---
+++ GlobalGameJam2020/Assets/Scripts/RepairController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repair : MonoBehaviour
+{
+    [SerializeField] private CircleCollider2D repairArea;
+    void Update()
+    {
+        if (Input.GetButtonDown("Repair"))
+        {
+            DoRepair();
+        }
+    }
+
+    private void DoRepair()
+    {
+        if(repairArea.enabled == false)
+        {
+            Debug.Log("Repairing");
+            repairArea.enabled = true;
+            StartCoroutine(TimeToDisableCollider(1f));
+        }
+    }
+
+    private IEnumerator TimeToDisableCollider(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        repairArea.enabled = false;
+    }
+
+
+}
