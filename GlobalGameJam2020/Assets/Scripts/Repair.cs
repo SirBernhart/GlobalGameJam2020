37e4117115494e796e0a89a457a4638548e469eb
Bernardo <behornerl@gmail.com>
2020-02-01 01:43:03 -0300

    Adicionando interação dummy com a barra de espaço

---
+++ GlobalGameJam2020/Assets/Scripts/Repair.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repair : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Repair"))
+        {
+            DoRepair();
+        }
+    }
+
+    private void DoRepair()
+    {
+        Debug.Log("Repairing");
+    }
+
+
+}
