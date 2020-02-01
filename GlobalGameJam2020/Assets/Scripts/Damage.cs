0bc8635963bbf873f608954406158f5a3fd99067
Bernardo <behornerl@gmail.com>
2020-02-01 04:23:12 -0300

    Criando o script de controle dos danos

---
+++ GlobalGameJam2020/Assets/Scripts/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    private int damageLevel;
+
+    public void ReduceDamage(int howMuch)
+    {
+        if(--damageLevel <= 0)
+        {
+            FinishRepairs();
+        }
+    }
+
+    private void FinishRepairs()
+    {
+        Destroy(gameObject);
+    }
+}
