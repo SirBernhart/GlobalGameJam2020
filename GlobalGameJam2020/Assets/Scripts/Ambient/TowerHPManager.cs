d6a9b4f7e4b4efdd05dd571eeba8ad68f0c104fc
Bernardo <behornerl@gmail.com>
2020-02-01 18:13:31 -0300

    Criando script que gerencia o HP da torre

---
+++ GlobalGameJam2020/Assets/Scripts/Ambient/TowerHPManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TowerHPManager : MonoBehaviour
+{
+    public float towerHP;
+    public float maxTowerHP;
+    public float HPToReduce;
+
+    [SerializeField] private Image healthBar;
+
+    public void Increase()
+    {
+
+    }
+
+    public void Reduce()
+    {
+        towerHP -= HPToReduce;
+        UpdateHealthBar();
+        if(towerHP <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = towerHP / maxTowerHP;
+    }
+
+    private void EndGame()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+}
