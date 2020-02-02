bd03cfa07d5a26467e5028abfd8aa80682a7a06e
mlhomsi <mlhomsi@gmail.com>
2020-02-02 12:04:19 -0300

    Voz

---
+++ GlobalGameJam2020/Assets/RandomizerVocal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizerVocal : MonoBehaviour
+{
+
+
+     public AudioSource randomSound;
+ 
+     public AudioClip[] Frases
+     ;
+     public AudioClip ClipAtual;
+     //public int index;
+
+
+    // Start is called before the first frame update
+     public void OnEnable() {
+
+        randomSound.clip = Frases[Random.Range(0, Frases.Length)];
+        randomSound.Play ();
+        // CallAudio ();
+    }
+
+
+
+       public void RandomSoundness()
+     {
+
+     }
+}
