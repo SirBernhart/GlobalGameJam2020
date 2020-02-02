73deb81b3b5eaa9c069988a16996e6d330ecd502
Bernardo <behornerl@gmail.com>
2020-02-02 09:05:00 -0300

    Balanceando a progressão de dificuldade

--- GlobalGameJam2020/Assets/Scripts/DifficultyManager.cs
+++ GlobalGameJam2020/Assets/Scripts/DifficultyManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DifficultyManager : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class DifficultyManager : MonoBehaviour
     [SerializeField]static public int nChunks = 0;
     [SerializeField]static public int dificuldade = 1;
     [SerializeField]static public float velocidade = 0.7f;
+    [SerializeField] private TextMeshProUGUI floorCounter;
 
     private static float frameSize = 10.81192f;
 
@@ -20,48 +22,87 @@ public class DifficultyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(nChunks <= 4)
+        floorCounter.text = nChunks.ToString();
+
+        // Intro
+        if (nChunks <= 4)
         {
             dificuldade = 1;
             velocidade = frameSize / 6;
             return;
         }
-        if(nChunks <= 9)
+        if (nChunks <= 9)
         {
             dificuldade = 2;
             velocidade = frameSize / 5;
             return;
         }
-        if(nChunks <= 16)
+        // Fácil
+        if (nChunks <= 13)
         {
             dificuldade = 3;
             velocidade = frameSize / 5;
             return;
         }
-        if(nChunks <= 26)
+        if (nChunks <= 17)
         {
             dificuldade = 3;
             velocidade = frameSize / 4.5f;
             return;
         }
-        if(nChunks <= 36)
+        if (nChunks <= 20)
         {
             dificuldade = 4;
             velocidade = frameSize / 4.5f;
+            return;
         }
-        if(nChunks <= 51)
+        if (nChunks <= 23)
         {
             dificuldade = 4;
             velocidade = frameSize / 4;
+            return;
         }
-        if(nChunks <= 80)
+        if (nChunks <= 26)
         {
             dificuldade = 5;
             velocidade = frameSize / 4;
+            return;
         }
-        dificuldade = 5;
-        velocidade = frameSize / 3.5f;
+        // Médio
+        if (nChunks <= 31)
+        {
+            dificuldade = 5;
+            velocidade = frameSize / 3.5f;
+            return;
+        }
+        if (nChunks <= 36)
+        {
+            dificuldade = 6;
+            velocidade = frameSize / 3.5f;
+            return;
+        }
+        // Difícil
+        if (nChunks <= 43)
+        {
+            dificuldade = 6;
+            velocidade = frameSize / 3f;
+            return;
+        }
+        if (nChunks <= 50)
+        {
+            dificuldade = 7;
+            velocidade = frameSize / 3f;
+            return;
+        }
+        // Duro de matar / Inferno / Elite / Somente os brabo
+        if (nChunks <= 57)
+        {
+            dificuldade = 7;
+            velocidade = frameSize / 2.5f;
+            return;
+        }
+        dificuldade = 7;
+        velocidade = frameSize / 2f;
         return;
-        
     }
 }
