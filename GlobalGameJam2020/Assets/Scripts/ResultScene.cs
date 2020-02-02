54c69fb02105786f312f90e6f6d15c80a8631060
Rapha <raraphaelar@gmail.com>
2020-02-02 10:51:13 -0300

    cool things

--- GlobalGameJam2020/Assets/Scripts/ResultScene.cs
+++ GlobalGameJam2020/Assets/Scripts/ResultScene.cs
@@ -6,24 +6,29 @@ using TMPro;
 public class ResultScene: MonoBehaviour
 {
     public TextMeshProUGUI field;
-    public int targetScore;
     public int currentDisplayScore = 0;
+    public int currentDisplayFloor = 0;
+    public ScoreSO result;
     
-    
+
 
     void Start()
     {
+
         StartCoroutine(CountUpToTarget());
     }
  
     IEnumerator CountUpToTarget()
     {
-        while (currentDisplayScore < targetScore)
+        while ((currentDisplayScore < result.score) && (currentDisplayFloor < result.floor))
         {
+            currentDisplayFloor += 1; 
+            currentDisplayFloor = (int)Mathf.Clamp(currentDisplayScore, 0f, result.score);
             currentDisplayScore += 3; // or whatever to get the speed you like
-            currentDisplayScore = (int)Mathf.Clamp(currentDisplayScore, 0f, targetScore);
-            field.text =  DifficultyManager.nChunks + " floors repaired! \n" + currentDisplayScore + " points collected! \n\nBetter luck next time";
+            currentDisplayScore = (int)Mathf.Clamp(currentDisplayScore, 0f, result.score);
+            field.text =  result.floor + " floors repaired! \n" + currentDisplayScore + " points collected! \n\nBetter luck next time";
             yield return null;
+    
         }
     }
  
