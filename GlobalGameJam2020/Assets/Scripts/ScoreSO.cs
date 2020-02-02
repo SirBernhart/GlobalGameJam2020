0d1faa7953777a54acd4e5376a352d2d8f081f20
NagibSuaid <nagibmoura@gmail.com>
2020-02-02 11:36:23 -0200

    Scriptable Object com score e floor

---
+++ GlobalGameJam2020/Assets/Scripts/ScoreSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScoreSO", menuName = "ScriptableObjects/ScoreSO")]
+public class ScoreSO : ScriptableObject
+{
+    public int score;
+    public int floor;
+}
